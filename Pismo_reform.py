bd71b7f056008b139d3f6f30061ecd923278cb47
D1srupt <vindigo01@gmail.com>
2024-05-06 15:44:53 +0600

    Сделал функционал с заменой ая и ый чекая json файл и находя соответствия

--- Pismo_reform.py
+++ Pismo_reform.py
@@ -3,10 +3,15 @@ from docx import Document
 from docx.shared import Pt, RGBColor
 import CTkMessagebox
 import os.path
+import json
 
 
 
 def on_document_pismo_select(second, fio, phn):
+
+    with open("female_patronymics.json", "r", encoding='utf-8') as file:
+        data = json.load(file)
+
     second.withdraw()
     CTk.set_default_color_theme("blue")
 
@@ -22,20 +27,26 @@ def on_document_pismo_select(second, fio, phn):
     address_entry = CTk.CTkEntry(third, width=300)
     address_entry.pack()
 
-    io_label = CTk.CTkLabel(third, text="Укажите имя и отчество адресата:")
+    io_label = CTk.CTkLabel(third, text="Укажите имя адресата:")
     io_label.pack()
 
     io_entry = CTk.CTkEntry(third, width=300)
     io_entry.pack()
 
-    gender_var = CTk.StringVar()
-    gender_var.set("Мужской")
-    male_radio = CTk.CTkRadioButton(third, text="Мужской", variable=gender_var, radiobutton_width=15,
-                                              radiobutton_height=15, value="Мужской")
-    male_radio.pack()
-    female_radio = CTk.CTkRadioButton(third, text="Женский", variable=gender_var, radiobutton_width=15,
-                                                radiobutton_height=15, value="Женский")
-    female_radio.pack()
+    o_label = CTk.CTkLabel(third, text="Укажите отчество адресата:")
+    o_label.pack()
+
+    o_entry = CTk.CTkEntry(third, width=300)
+    o_entry.pack()
+
+    # gender_var = CTk.StringVar()
+    # gender_var.set("Мужской")
+    # male_radio = CTk.CTkRadioButton(third, text="Мужской", variable=gender_var, radiobutton_width=15,
+    #                                           radiobutton_height=15, value="Мужской")
+    # male_radio.pack()
+    # female_radio = CTk.CTkRadioButton(third, text="Женский", variable=gender_var, radiobutton_width=15,
+    #                                             radiobutton_height=15, value="Женский")
+    # female_radio.pack()
 
     theme_label = CTk.CTkLabel(third, text="Введите тему:")
     theme_label.pack()
@@ -81,11 +92,11 @@ def on_document_pismo_select(second, fio, phn):
     select_button.pack()
     select_button.place(x=110, y=615)
 
-    def sz_reform_def():
+    def sz_reform_def(patronymic):
         answer = answer_entry.get()
-        pol = gender_var.get()
+        # pol = gender_var.get()
         color = RGBColor(91, 155, 213)
-        io = io_entry.get()
+        io = io_entry.get() + ' ' + o_entry.get()
         addr = addr_entry.get()
         theme = theme_entry.get()
         choice = address_entry.get()
@@ -123,20 +134,21 @@ def on_document_pismo_select(second, fio, phn):
         # Добавим замену для текста, не находящегося в таблице
         for paragraph in docu.paragraphs:
             paragraph.text = paragraph.text.replace("(Адресат)", choice)
-            if pol == "Мужской":
-                paragraph.text = paragraph.text.replace("(Пол)", "ый")
-            elif pol == "Женский":
+            if patronymic in data["Женский"]:
                 paragraph.text = paragraph.text.replace("(Пол)", "ая")
+            else:
+                paragraph.text = paragraph.text.replace("(Пол)", "ый")
             if "(Имя и Отчество)" in paragraph.text:
                 paragraph.text = paragraph.text.replace("(Имя и Отчество)", io)
             if "(Содержание)" in paragraph.text:
                 paragraph.text = paragraph.text.replace("(Содержание)", content)
 
+
         docu.save(save_path)
         os.remove("Temp.docx")
         CTkMessagebox.CTkMessagebox(title="Docs Generator", message="Документ успешно сформирован!")
 
-    generate_button = CTk.CTkButton(third, text="Сформировать документ", command=sz_reform_def)
+    generate_button = CTk.CTkButton(third, text="Сформировать документ", command=lambda: sz_reform_def(o_entry.get()))
     generate_button.pack()
     generate_button.place(x=117, y=650)
 
