4756b9129af7151cc55e24ef0b2993de50f10c05
D1srupt <vindigo01@gmail.com>
2024-05-08 13:34:49 +0600

    Отпалировал служебку

--- SZ_reform.py
+++ SZ_reform.py
@@ -16,20 +16,39 @@ def on_document_sz_select(root, second, fio, phn):
         "400x750+{}+{}".format(int(third.winfo_screenwidth() / 2 - 150), int(third.winfo_screenheight() / 2 - 300)))
     third.resizable(width=False, height=False)
     third.title("Служебная записка")
+    third.iconbitmap('капля.ico')
 
     address_label = CTk.CTkLabel(third, text="Выберите адресата:")
     address_label.pack()
 
     address_var = CTk.StringVar()
-    address_combobox = CTk.CTkComboBox(third, width=300, variable=address_var,
-                                       values=["Генеральный директор", "Главный инженер",
-                                               "Директор по безопасности"])
+    address_combobox = CTk.CTkComboBox(third, width=300, variable=address_var, corner_radius=4, border_width=1,
+                                       border_color="#1f6aa5", button_hover_color='#1f6aa5',
+                                       dropdown_hover_color='#1f6aa5',
+                                       values=["Генеральный директор",
+                                               "Главный инженер",
+                                               "Директор по безопасности",
+                                               "Директор по персоналу",
+                                               "Начальник технического отдела",
+                                               "Начальник отдела оптимизации режимов сетей и сооружений",
+                                               "Начальник службы АСУ ТП и Метрологии",
+                                               "Начальник производственного отдела",
+                                               "Заместитель генерального директора Производственной дирекции",
+                                               "Начальник центральной диспетчерской службы",
+                                               "Заместитель генерального директора Дирекции по капитальному строительству",
+                                               "Коммерческий директор",
+                                               "Директор по экономике",
+                                               "Заместитель генерального директора Дирекции по правовым вопросам",
+                                               "Начальник отдела контроллинга",
+                                               "Начальник отдела информационных технологий",
+                                               "Начальник управления системой снабжения"])
     address_combobox.pack()
 
     theme_label = CTk.CTkLabel(third, text="Введите тему:")
     theme_label.pack()
 
-    theme_entry = CTk.CTkEntry(third, width=300)
+    theme_entry = CTk.CTkEntry(third, width=300, corner_radius=4, border_width=1,
+                               border_color="#1f6aa5")
     theme_entry.pack()
 
     content_label = CTk.CTkLabel(third, text="Введите содержание:")
@@ -38,26 +57,30 @@ def on_document_sz_select(root, second, fio, phn):
     def on_paste():
         content_entry.paste()
 
-    content_entry = CTk.CTkTextbox(third, height=200, width=300)
+    content_entry = CTk.CTkTextbox(third, height=200, width=300, corner_radius=4, border_width=1,
+                                   border_color="#1f6aa5", fg_color='gray19')
     content_entry.bind('<Control-v>', on_paste)
     content_entry.pack()
 
     save_path_label = CTk.CTkLabel(third, text="Выберите место сохранения:")
     save_path_label.pack()
 
-    save_path_entry = CTk.CTkEntry(third, width=50)
+    save_path_entry = CTk.CTkEntry(third, width=300, corner_radius=4, border_width=1,
+                                   border_color="#1f6aa5")
     save_path_entry.pack()
-    save_path_entry.place(x=110, y=370)
+    save_path_entry.place(x=50, y=370)
 
     def select_save_path():
         save_path = CTk.filedialog.asksaveasfilename(defaultextension=".docx",
                                                      filetypes=[("Word files", "*.docx")])
         save_path_entry.delete(0, "end")
         save_path_entry.insert(0, save_path)
+        save_path_entry.configure(state='disabled')
+        generate_button.configure(state='normal')
 
-    select_button = CTk.CTkButton(third, text="Выбрать место сохранения", command=select_save_path)
+    select_button = CTk.CTkButton(third, text="Выбрать место сохранения", corner_radius=10, command=select_save_path)
     select_button.pack()
-    select_button.place(x=110, y=370)
+    select_button.place(x=110, y=420)
 
     def sz_reform_def():
         theme = theme_entry.get()
@@ -96,6 +119,81 @@ def on_document_sz_select(root, second, fio, phn):
                                 run = paragraph.runs[0]
                                 run.font.name = "Arial"
                                 run.font.size = Pt(14)
+                            elif choice == "Директор по персоналу":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Директору по персоналу")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник технического отдела":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику технического отдела")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Главный технолог":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Главному технологу")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела оптимизации режимов сетей и сооружений":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику отдела оптимизации режимов сетей и сооружений")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник службы АСУ ТП и Метрологии":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику службы АСУ ТП и Метрологии")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник производственного отдела":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику производственного отдела")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Производственной дирекции":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Заместителю генерального директора Производственной дирекции")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник центральной диспетчерской службы":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику центральной диспетчерской службы")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Дирекции по капитальному строительству":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Заместителю генерального директора Дирекции по капитальному строительству")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Коммерческий директор":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Коммерческому директору")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Директор по экономике":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Директору по экономике")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Дирекции по правовым вопросам":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Заместителю генерального директора Дирекции по правовым вопросам")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела контроллинга":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику отдела контроллинга")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела информационных технологий":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику отдела информационных технологий")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник управления системой снабжения":
+                                paragraph.text = paragraph.text.replace("(Адресат)", "Начальнику управления системой снабжения")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
 
                         if "(Имя и Отчество)" in paragraph.text:
                             if choice == "Генеральный директор":
@@ -110,6 +208,81 @@ def on_document_sz_select(root, second, fio, phn):
                                 run.font.size = Pt(14)
                             elif choice == "Директор по безопасности":
                                 paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Андрей Владимирович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Директор по персоналу":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Анна Анатольевна")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник технического отдела":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Сергей Анатольевич")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Главный технолог":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Евгений Борисович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела оптимизации режимов сетей и сооружений":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Денис николаевич")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник службы АСУ ТП и Метрологии":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Александр Олегович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник производственного отдела":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Ирина Александровна")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Производственной дирекции":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Сергей Владимирович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник центральной диспетчерской службы":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Дмитрий Григорьевич")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Дирекции по капитальному строительству":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Евгений Гарьевич")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Коммерческий директор":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Ольга Юрьевна")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Директор по экономике":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Михаил Александрович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Заместитель генерального директора Дирекции по правовым вопросам":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Дмитрий Анатольевич")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела контроллинга":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Оксана Романовна")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник отдела информационных технологий":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Сергей Вадимович")
+                                run = paragraph.runs[0]
+                                run.font.name = "Arial"
+                                run.font.size = Pt(14)
+                            elif choice == "Начальник управления системой снабжения":
+                                paragraph.text = paragraph.text.replace("(Имя и Отчество)", "Дмитрий Александрович")
                                 run = paragraph.runs[0]
                                 run.font.name = "Arial"
                                 run.font.size = Pt(14)
@@ -180,10 +353,10 @@ def on_document_sz_select(root, second, fio, phn):
         if msg.get() == 'Нет':
             sys.exit()
 
-    generate_button = CTk.CTkButton(third, text="Сформировать документ", command=sz_reform_def)
+    generate_button = CTk.CTkButton(third, text="Сформировать документ", command=sz_reform_def, state='disabled',
+                                    width=200, height=70, fg_color='#c52222', hover_color='#9d1b1b', corner_radius=10)
     generate_button.pack()
-    generate_button.place(x=117, y=650)
-
+    generate_button.place(x=105, y=550)
 
     def on_closing():
         sys.exit()
@@ -198,4 +371,3 @@ def on_document_sz_select(root, second, fio, phn):
 
     third.protocol("WM_DELETE_WINDOW", on_closing)
     third.mainloop()
-
